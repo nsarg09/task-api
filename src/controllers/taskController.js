e04bfdeee1361a7f0c4787319f4d7925f2fd8f9e
Nisarg Patel <nisarg@Mac.lan>
2026-04-14 22:02:07 -0400

    add completed query filtering to tasks endpoint

--- src/controllers/taskController.js
+++ src/controllers/taskController.js
@@ -1,7 +1,20 @@
 import * as taskService from '../services/taskService.js';
 
 export async function getTasks(req, res, next) {
-  const tasks = await taskService.getAllTasks();
+  const { completed } = req.query;
+
+  if(completed !== undefined && completed !== 'true' && completed !== 'false') {
+    return res.status(400).json({ error: 'Invalid value for completed query parameter' });
+  }
+
+  let filter = undefined;
+  if (completed === 'true') {
+    filter = true;
+  } else if (completed === 'false') {
+    filter = false;
+  }
+
+  const tasks = await taskService.getAllTasks(filter);
   res.json(tasks);
 }
 
